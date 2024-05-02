564d6f7516f2ea06ada7b05bb0d8ad4b22a95a40
Chandradeep Roy <140598046+nirvair09@users.noreply.github.com>
2024-05-02 21:42:28 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use("/test", (req, res) => {
+app.use("/", (req, res) => {
   res.send("Hello world!");
 });
 
