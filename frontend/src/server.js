3e912f64d48372e4f39b4a19d06a7bedc2aa9646
Chandradeep Roy <140598046+nirvair09@users.noreply.github.com>
2024-05-02 21:48:24 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = 'https://sde-project-ecom-backend.onrender.com/api/v2';
+export const server = 'https://sde-project-e-commerce.onrender.com/api/v2';
 
-export const backend_url = "https://sde-project-ecom-backend.onrender.com";
+export const backend_url = "https://sde-project-e-commerce.onrender.com";
 
